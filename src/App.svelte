d393db4d53a7e96514360a33b80b831321d6dd13
Christian Consuelo <christian.consuelo2@gmail.com>
2020-06-13 13:12:44 -0500

    Add basic game functionality

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,13 @@
 <script>
 import Board from './containers/Board.svelte';
-import { cards } from './store/store';
+import Modal from './containers/Modal.svelte';
+
+import { cards, foundCards } from './store/store';
 import { generateRandomPairsArrangement } from './helpers/helpers';
 
 const handleStartGame = () => {
 	cards.update(generateRandomPairsArrangement);
+	foundCards.update(() => []);
 };
 </script>
 
@@ -14,11 +17,30 @@ const handleStartGame = () => {
 	align-items: center;
 	height: 90vh;
 }
+
+.Intro-start-game {
+  height: 150px;
+  width: 150px;
+  border: 1px solid white;
+  border-radius: 50%;
+  color: white;
+  background-color: transparent;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  font-size: 24px;
+  font-weight: 600;
+  cursor: pointer;
+  transition: transform 0.2s;
+}
+
 </style>
 
 <main>
-	<button on:click={handleStartGame}>Start Game</button>
-	<div class="App">
-		<Board />
-	</div>
+	<Board />
+	<Modal visible={$cards.length === 0 || $cards.length === $foundCards.length}>
+		<div>
+			<button class="Intro-start-game" on:click={handleStartGame}>Start Game</button>
+		</div>
+	</Modal>
 </main>
---
+++ src/containers/Modal.svelte
@@ -0,0 +1,48 @@
+<script>
+export let visible;
+</script>
+
+<style>
+.Modal {
+  position: fixed;
+  left: 0px;
+  top: 0px;
+  right: 0px;
+  bottom: 0px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  pointer-events: none;
+}
+
+.Modal-content {
+  align-items: center;
+  background: rgba(0, 0, 0, 0.5);
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  overflow: hidden;
+  transition-property: width, height;
+  transition-duration: 0.5s;
+  pointer-events: all;
+}
+
+.Modal-content--hidden {
+  width: 0px;
+  height: 0px;
+  border-radius: 50%;
+}
+
+.Modal-content--visible {
+  width: 100vw;
+  height: 100vw;
+  border-radius: 0;
+}
+
+</style>
+
+<div class="Modal">
+  <div class={`Modal-content ${visible ? 'Modal-content--visible' : 'Modal-content--hidden'}`}>
+    <slot />
+  </div>
+</div>
